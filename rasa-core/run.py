ce61ac037728f1aec4deb1082b50a86f8d3d2d34
Sarwesh Suman <sarweshsuman.1@gmail.com>
2017-10-05 12:38:42 +0530

    Update run.py

--- rasa-core/run.py
+++ rasa-core/run.py
@@ -11,25 +11,14 @@ from rasa_dm.policies.memoization import MemoizationPolicy
 from rasa_dm.channels.console import ConsoleInputChannel
 from rasa_dm.tracker_store import InMemoryTrackerStore
 from rasa_dm.actions.action import ACTION_LISTEN_NAME
+import sys
 
 import logging
 logger = logging.getLogger(__name__)
 logger.setLevel(10)
 
-class SimplePolicy(Policy):
-    def predict_action_probabilities(self, tracker, domain):
-        # type: (DialogueStateTracker, Domain) -> List[float]
-        if tracker.latest_action_id_str == ACTION_LISTEN_NAME:
-            print(tracker.latest_message.entities)
-            key = tracker.latest_message.intent["name"]
-            action = domain.action_map['action_'+key][0]
-            return one_hot(action, domain.num_actions)
-        else:
-            print("in else part")
-            return np.zeros(domain.num_actions)
-
-full_config_path = "/home/cdpai/online_implementation_of_chatbot/rasa/config_spacy.json"
-full_model_path = "/home/cdpai/online_implementation_of_chatbot/rasa/models/model_20170918-060638"
+full_config_path = sys.argv[1]
+full_model_path = sys.argv[2]
 
 metadata = Metadata.load(full_model_path)
 interpreter1 = Interpreter.load(metadata,RasaNLUConfig(full_config_path))
